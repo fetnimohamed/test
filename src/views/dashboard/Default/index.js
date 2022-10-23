aed3a6ad518ec71a497f25b2c5dca27f85b520a9
elfetnimohamed94 <elfetnimohamed94@gmail.com>
2022-10-23 16:24:54 +0100

    feat(init): this is init for the test
    this is the template initialization
    
    BREAKING CHANGE: everything is new

---
+++ src/views/dashboard/Default/index.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react';
+import * as React from 'react';
+// material-ui
+import { Grid, Card, Box, CardActions, CardContent, Typography, Button } from '@mui/material';
+
+// project imports
+import EarningCard from './EarningCard';
+import PopularCard from './PopularCard';
+import TotalOrderLineChartCard from './TotalOrderLineChartCard';
+import TotalIncomeDarkCard from './TotalIncomeDarkCard';
+import TotalIncomeLightCard from './TotalIncomeLightCard';
+import TotalGrowthBarChart from './TotalGrowthBarChart';
+import { gridSpacing } from 'store/constant';
+
+// ==============================|| DEFAULT DASHBOARD ||============================== //
+
+const Dashboard = () => {
+    const [isLoading, setLoading] = useState(true);
+    useEffect(() => {
+        setLoading(false);
+    }, []);
+    const bull = (
+        <Box component="span" sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}>
+            •
+        </Box>
+    );
+
+    const card = (
+        <React.Fragment>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Word of the Day
+                </Typography>
+                <Typography variant="h5" component="div">
+                    be{bull}nev{bull}o{bull}lent
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    adjective
+                </Typography>
+                <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </React.Fragment>
+    );
+
+    return (
+        <Grid container spacing={gridSpacing}>
+            <Grid item xs={4}>
+                <Card variant="outlined">{card}</Card>
+            </Grid>
+            <Grid item xs={12}>
+                <Grid container spacing={gridSpacing}>
+                    <Grid item xs={12} md={8}>
+                        <TotalGrowthBarChart isLoading={isLoading} />
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                        <PopularCard isLoading={isLoading} />
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default Dashboard;
